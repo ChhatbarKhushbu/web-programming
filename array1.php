7d963a908cd1b61cf97bcd9838c60518790fed0b
ChhatbarKhushbu <155127117+ChhatbarKhushbu@users.noreply.github.com>
2024-01-25 08:36:11 +0530

    Add files via upload

---
+++ array1.php
@@ -0,0 +1,13 @@
+<?php
+
+$student=array(13,"khushbu","mahek","prakruti","ruchi","disha","richa","prushti","sakshi","twisha");
+foreach($student as $myvalue)
+{
+print_r($myvalue);
+echo "<br>";
+}
+echo "<br>";
+echo ($student[7]);
+
+?>
+
